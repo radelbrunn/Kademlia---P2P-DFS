a71bff7eab0b858251155bb6d5d21018bf5d692b
maxkha-3 <maxkha-3@student.ltu.se>
2018-09-18 16:07:12 +0200

    Added a test file, which creates a contact and prints it

---
+++ main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"Kademlia---P2P-DFS/d7024e"
+	"fmt"
+)
+
+func main() {
+	contact := d7024e.NewContact(d7024e.NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000")
+	fmt.Print(contact)
+}
