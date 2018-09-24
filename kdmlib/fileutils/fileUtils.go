6a900259ee545f314360d7f251eb63f67bcdc7b5
jeremy <facchettos@gmail.com>
2018-09-24 18:13:12 +0200

    added test file, restructured some functions to be able to test it wihtout spawn new goroutines

--- kdmlib/fileutils/fileUtils.go
+++ kdmlib/fileutils/fileUtils.go
@@ -4,9 +4,10 @@ import (
 	"fmt"
 	"time"
 	"io/ioutil"
-	"log"
 	"os"
 	"sync"
+	"log"
+	"path/filepath"
 )
 
 const fileDirectory = ".files"
@@ -30,7 +31,7 @@ func checkIfInList(pinnedFiles map[string]bool, name string) bool {
 
 //creates a directory to put files in
 func createFilesDirectory(){
-	os.Mkdir(fileDirectory,0644)
+	os.Mkdir(fileDirectory,0755)
 }
 
 //add or removes files from the node
@@ -38,12 +39,14 @@ func fileHandler(order Order) {
 	if order.action == ADD {
 
 		//checks if the file is already present
-		if _, err := os.Stat(order.name); os.IsNotExist(err) {
+		if _, err := os.Stat(fileDirectory+string(filepath.Separator)+order.name); os.IsNotExist(err) {
 			err := ioutil.WriteFile(fileDirectory+string(os.PathSeparator)+order.name, order.content, 0644)
 			if err != nil {
 				fmt.Println("something went wrong while creating file " + order.name)
 			}
+			fmt.Println("no update")
 		}else{
+			fmt.Println("update")
 			//only updates the file's modification date if it is already present
 			updateFile(order.name)
 		}
@@ -64,21 +67,43 @@ func fileHandlerWorker(orders chan Order){
 }
 
 
+func pinFile (pinnedFiles *pinnedFilesStruct,ordersFromchan Order){
+	pinnedFiles.lock.Lock()
+	if ordersFromchan.action == ADD {
+		if !checkIfInList(pinnedFiles.pinnedFiles, ordersFromchan.name)  {
+			pinnedFiles.pinnedFiles[ordersFromchan.name]=true
+		}
+	} else if ordersFromchan.action == REMOVE {
+		if checkIfInList(pinnedFiles.pinnedFiles, ordersFromchan.name) {
+			pinnedFiles.pinnedFiles[ordersFromchan.name]=false
+		}
+	}
+	pinnedFiles.lock.Unlock()
+}
+
 //reads from the channel and pin or unpin according to the order
 
 func pinner(orders <-chan Order, pinnedFiles *pinnedFilesStruct) {
 	for ordersFromchan := range orders {
-		pinnedFiles.lock.Lock()
-		if ordersFromchan.action == ADD {
-			if !checkIfInList(pinnedFiles.pinnedFiles, ordersFromchan.name)  {
-				pinnedFiles.pinnedFiles[ordersFromchan.name]=true
+		pinFile(pinnedFiles,ordersFromchan)
+	}
+}
+
+func removeOldFiles( pinnedFiles *pinnedFilesStruct){
+	files, err := ioutil.ReadDir(fileDirectory)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		if f.ModTime().Before(time.Now().Add(-time.Hour * 25)) {
+			pinnedFiles.lock.Lock()
+			if !f.IsDir() && !checkIfInList(pinnedFiles.pinnedFiles, f.Name()) {
+				fmt.Println(f.Name(), " : file too old, thus removing it")
+				os.Remove(fileDirectory+string(os.PathSeparator)+f.Name())
 			}
-		} else if ordersFromchan.action == REMOVE {
-			if checkIfInList(pinnedFiles.pinnedFiles, ordersFromchan.name) {
-				pinnedFiles.pinnedFiles[ordersFromchan.name]=false
-			}
+			pinnedFiles.lock.Unlock()
 		}
-		pinnedFiles.lock.Unlock()
 	}
 }
 
@@ -86,21 +111,7 @@ func pinner(orders <-chan Order, pinnedFiles *pinnedFilesStruct) {
 
 func cleaner(pinnedFiles *pinnedFilesStruct) {
 	for {
-		files, err := ioutil.ReadDir(fileDirectory)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		for _, f := range files {
-			if f.ModTime().Before(time.Now().Add(-time.Hour * 25)) {
-				pinnedFiles.lock.Lock()
-				if !f.IsDir() && checkIfInList(pinnedFiles.pinnedFiles, f.Name()) {
-					fmt.Println(f.Name(), " : file too old, thus removing it")
-					os.Remove(fileDirectory+string(os.PathSeparator)+f.Name())
-				}
-				pinnedFiles.lock.Unlock()
-			}
-		}
+		removeOldFiles(pinnedFiles)
 		time.Sleep(time.Hour)
 	}
 }
