5a959dd3750e18c8c89104ab8fd9f7612f7a76af
maxkha-3 <maxkha-3@student.ltu.se>
2018-10-15 23:12:03 +0200

    Added test for a successful FindData

--- kdmlib/kademlia_lookupdata_test.go
+++ kdmlib/kademlia_lookupdata_test.go
@@ -2,11 +2,14 @@ package kdmlib
 
 import (
 	"Kademlia---P2P-DFS/kdmlib/fileutils"
+	"fmt"
+	"io/ioutil"
+	"os"
 	"testing"
 	"time"
 )
 
-func TestKademlia_LookupData(t *testing.T) {
+func TestKademlia_LookupDataFail(t *testing.T) {
 	nodeId1 := "10010000"
 	port1 := "12000"
 	nodeId2 := "10110000"
@@ -76,4 +79,64 @@ func TestKademlia_LookupData(t *testing.T) {
 		t.Error("Expected data return to fail")
 		t.Fail()
 	}
+}
+
+func TestKademlia_LookupDataSuccess(t *testing.T) {
+
+	os.Mkdir(".files/", 0755)
+	fileName := "11111111"
+	ioutil.WriteFile(".files"+string(os.PathSeparator)+fileName, []byte("hello world"), 0644)
+
+	nodeId1 := "00000011"
+	port1 := "12002"
+	nodeId2 := "00000100"
+	port2 := "12003"
+
+	targetData := "11111111"
+
+	testContacts := []AddressTriple{
+		{"127.0.0.1", "12003", "00000100"}, {"127.0.0.1", "12001", "00000001"},
+		{"127.0.0.1", "12002", "00000011"}, {"127.0.0.1", "12000", "10010000"},
+		{"127.0.0.1", "12004", "01100000"}, {"127.0.0.1", "12005", "11110000"},
+		{"127.0.0.1", "12006", "11010000"}, {"127.0.0.1", "12007", "11111101"},
+		{"127.0.0.1", "12008", "01110000"}, {"127.0.0.1", "12009", "11110001"},
+		{"127.0.0.1", "12010", "11110010"}, {"127.0.0.1", "12011", "11110011"},
+		{"127.0.0.1", "12012", "11110100"}, {"127.0.0.1", "12013", "11110101"},
+		{"127.0.0.1", "12014", "11110110"}, {"127.0.0.1", "12015", "11110111"},
+		{"127.0.0.1", "12016", "11111000"}, {"127.0.0.1", "12017", "11111001"},
+		{"127.0.0.1", "12018", "11111010"}, {"127.0.0.1", "12019", "11111011"},
+		{"127.0.0.1", "12020", "11111100"}, {"127.0.0.1", "13000", "10110000"}}
+
+	rt1 := CreateAllWorkersForRoutingTable(K, 8, 5, nodeId1)
+	for _, e := range testContacts[0:11] {
+		rt1.GiveOrder(OrderForRoutingTable{ADD, e, false})
+	}
+
+	time.Sleep(time.Second * 1)
+
+	rt2 := CreateAllWorkersForRoutingTable(K, 8, 5, nodeId2)
+	for _, e := range testContacts[1:9] {
+		rt2.GiveOrder(OrderForRoutingTable{ADD, e, false})
+	}
+
+	time.Sleep(time.Second * 1)
+
+	chanPin1, chanFile1, fileMap1 := fileUtilsKademlia.CreateAndLaunchFileWorkers()
+	chanPin2, chanFile2, fileMap2 := fileUtilsKademlia.CreateAndLaunchFileWorkers()
+
+	InitNetwork(port1, "127.0.0.1", rt1, nodeId1, false, chanFile1, chanPin1, fileMap1)
+	nw2 := InitNetwork(port2, "127.0.0.1", rt2, nodeId2, true, chanFile2, chanPin2, fileMap2)
+
+	testKademlia := NewKademliaInstance(nw2, nodeId2, ALPHA, K, rt2, chanFile2, fileMap2)
+
+	dataReturn := testKademlia.LookupData(targetData, true)
+
+	fmt.Println("Data returned: ", string(dataReturn))
+
+	if dataReturn == nil {
+		t.Error("Expected to get data back...")
+		t.Fail()
+	}
+
+	os.Remove(".files" + string(os.PathSeparator) + "11111111")
 }
